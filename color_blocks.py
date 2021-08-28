58270901f6de8ef4e2796cdc971ab2ecd32bfb2c
sparkz11 <noahvendrig@gmail.com>
2021-08-29 00:04:42 +1000

    working but bad quality

--- color_blocks.py
+++ color_blocks.py
@@ -1,25 +1,60 @@
 import pandas as pd
 import cv2
-
+import math
 from PIL import Image
 import numpy as np
-
+import os
 # pandas stuff
+
+
 DATA_CSV = pd.read_csv('data.csv')
-print(DATA_CSV)
+# print(DATA_CSV)
+blocks_rgb = DATA_CSV["RGB"]
+# d= d.split()
+# blocks_rgb2 = tuple(map(int,blocks_rgb.split(',')))
 
 
+# print(blocks_rgb)
 
+def Find_Euclidian_Distance(pxColour, blockColour):
+    distance = math.sqrt((pxColour[0] - blockColour[0])**2 + (pxColour[1] - blockColour[1])**2 + (pxColour[2] - blockColour[2])**2)
+    return distance
 
+def Find_Colour_Match(pxColour, DATA_CSV=DATA_CSV):
+    closest_distance = 473
+    closest_colour = None
+    for colour in blocks_rgb:
+        blockColour = tuple(map(int,colour.split(',')))
+        colour_distance = Find_Euclidian_Distance(pxColour, blockColour)
 
+        if colour_distance < closest_distance:
+            closest_colour, closest_distance = colour, colour_distance
+
+    # print(closest_colour)
+    indexMatch = DATA_CSV[DATA_CSV['RGB']==closest_colour].index
+    # colourMatch = DATA_CSV['file_name'][indexMatch]
+
+    return indexMatch
+
+# pxColour = (42,35,42)
+# cd = Find_Colour_Match(pxColour)
+# print(cd)
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
 boxLength = 4
 boxSize = boxLength* boxLength
 
-
-
 # Open Paddington and make sure he is RGB - not palette
-im = Image.open('input/2.jpg').convert('RGB')
+IMG_NAME = '4.jpg'
+im = Image.open(f"input/{IMG_NAME}").convert('RGB')
 width, height = im.size
+
+if width > 1000 or height > 1000:
+    w2 = int(width/8)
+    h2 = int(height/8)
+    im = im.resize((w2,h2))
+    width, height = im.size
+
 colours = []
 
 def adjustSize(dimension, base=boxSize):
@@ -35,13 +70,16 @@ print(f'{width = }, {height}')
 
 boxPoints = []
 
-for x in range(0, width-boxLength, boxLength):
+wScale = 2
+
+for x in range(0, width-(boxLength), boxLength):
     
-    for y in range(0, height-(2*boxLength), boxLength):
+    for y in range(0, height-(wScale*boxLength), boxLength):
         origin = (x,y)
         boxPoints.append(origin)
 
-   
+print(boxPoints[-4:])
+
 for pt in boxPoints:
     # print(pt)
     x,y = pt
@@ -53,6 +91,7 @@ for pt in boxPoints:
     ave_b = 0
     for px in range(x-1,x+3):
         for py in range(y-1, y+3):
+            # print(pt, "d:",px,py)
             colours.append(im.getpixel((px,py)))
             # print(px,py)
     for rgb in colours:
@@ -61,9 +100,20 @@ for pt in boxPoints:
         ave_b += rgb[2]
     avgRGB = int(ave_r/boxSize),int(ave_g/boxSize),int(ave_b/boxSize) # find the average rgb OF THE 4*4 box
 
+    
+    indexMatch = list(Find_Colour_Match(avgRGB))[0] # find the block that matches the colours of the avg pixel rgb. convert it to a string for ease of use later
+    nameMatch = DATA_CSV['file_name'][indexMatch] # useless but good to see the names in development
+    matchImgPath = DATA_CSV['img_location'][indexMatch] # get the path of the img - we need this bit
+    # print(nameMatch)
+    # print(f"{matchImgPath = }")
+
+    currBlockImg = Image.open(matchImgPath).convert('RGB')
+    currColour = None
+
     for px in range(x-1,x+3): # Change the colour to the average
         for py in range(y-1, y+3):
-            new.putpixel((px,py), avgRGB) 
+            currColour = currBlockImg.getpixel((px-x,py-y))
+            new.putpixel((px,py), currColour) 
 
 
                 
@@ -84,10 +134,18 @@ for pt in boxPoints:
 # for x in range(pt[0], pt[0]+boxLength):
 #     for y in range(pt[1], pt[1]+boxLength):
 #         im.putpixel((x,y), avgRGB)
+new = new.resize((width*10,height*10))
 
-   
-# new.show()
-new.show()
+dir = os.listdir("./output")
+
+if len(dir) == 0:
+    num = 1
+else:
+    prevName = str(max(dir))
+    num = int(list(prevName)[-5])+1
+new = new.save(f"./output/{IMG_NAME}")
+
+
 # new.save('output/1.png')
 #         new.putpixel((x, y), (255, 255, 255))
         # r,g,b = im.getpixel((x,y))
--- trash/test.py
+++ trash/test.py
@@ -25,15 +25,26 @@
 
 # print(myround(x))
 
-from PIL import Image
-im = Image.open('img/birch_trapdoor.png').convert('RGB')
-w,h = im.size
-for x in range(w):
-    for y in range(h):
-        r,g,b = im.getpixel((x,y))
+# from PIL import Image
+# im = Image.open('img/birch_trapdoor.png').convert('RGB')
+# w,h = im.size
+# for x in range(w):
+#     for y in range(h):
+#         r,g,b = im.getpixel((x,y))
 
-im.show()
+# im.show()
 
 # for px in range(x+1,x+4):
 #             for py in range(y+1, y+4):
 #                 new.putpixel((px,py), (255,0,255))
+
+# d = ["output1.png","output2.png","output3.png","output4.png"]
+# print(max(d))
+
+from PIL import Image
+im = Image.open(f"input/4.jpg").convert('RGB')
+w,h = im.size
+w = w/8
+h = h/8
+im = im.resize()
+new = new.save(f"./output/{IMG_NAME}")
