b2c8799f74717458d4c8ecf3e6f09a2778fc1c5f
sparkz11 <noahvendrig@gmail.com>
2021-08-28 18:16:58 +1000

    fixed

--- color_blocks.py
+++ color_blocks.py
@@ -4,22 +4,18 @@ import cv2
 from PIL import Image
 import numpy as np
 
+# pandas stuff
 DATA_CSV = pd.read_csv('data.csv')
+print(DATA_CSV)
 
-# img = cv2.imread('./input/1.png')
-# img_width= img.shape[1]
-# img_height= img.shape[0]
-# img= cv2.resize(img, (img_width*16, img_height*16))
-# print(img_width, img_height)
-# img[0:15, 0:15] = (0,0,0)
-# # img[0:50, 0:50] = "img/birch_trapdoor.png"
-    
-# cv2.imshow('image', img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+
+
+
 
 boxLength = 4
 boxSize = boxLength* boxLength
+
+
 
 # Open Paddington and make sure he is RGB - not palette
 im = Image.open('input/2.jpg').convert('RGB')
@@ -28,47 +24,70 @@ colours = []
 
 def adjustSize(dimension, base=boxSize):
     return base * round(float(dimension) / base)
-# width = adjustSize(width) * 16
-# height =adjustSize(height) * 16
+width = (adjustSize(width)) #* 16
+height = (adjustSize(height)) #* 16
 
 im.resize((width,height))
 
-new = Image.new(mode="RGB", size=(width, height))
+new = Image.new("RGB", (width, height),(255, 255, 255))
 
-print(width,height)   
+print(f'{width = }, {height}')
 
 boxPoints = []
 
-for x in range(0, width, boxLength):
+for x in range(0, width-boxLength, boxLength):
     
-    for y in range(0, height, boxLength):
+    for y in range(0, height-(2*boxLength), boxLength):
         origin = (x,y)
         boxPoints.append(origin)
-        # print(colours)
-        colours = []
-        ave_r = 0
-        ave_g = 0
-        ave_b = 0
-        newColour = int(ave_r/boxSize),int(ave_g/boxSize),int(ave_b/boxSize)
-        # print(newColour)
-        
-        for px in range(x,x+3):
-            for py in range(y, y+3):
-                colours.append(im.getpixel((px,py)))
-                # print(px,py)
-                for rgb in colours:
-                    ave_r += rgb[0]
-                    ave_g += rgb[1]
-                    ave_b += rgb[2]
-                im.putpixel((px,py), (newColour))
+
+   
+for pt in boxPoints:
+    # print(pt)
+    x,y = pt
+    # print("-"*50)
+    # print(pt)
+    colours = []
+    ave_r = 0
+    ave_g = 0
+    ave_b = 0
+    for px in range(x-1,x+3):
+        for py in range(y-1, y+3):
+            colours.append(im.getpixel((px,py)))
+            # print(px,py)
+    for rgb in colours:
+        ave_r += rgb[0]
+        ave_g += rgb[1]
+        ave_b += rgb[2]
+    avgRGB = int(ave_r/boxSize),int(ave_g/boxSize),int(ave_b/boxSize) # find the average rgb OF THE 4*4 box
+
+    for px in range(x-1,x+3): # Change the colour to the average
+        for py in range(y-1, y+3):
+            new.putpixel((px,py), avgRGB) 
 
 
                 
-print(boxPoints)         
+
+    # im.putpixel((pt[0],pt[1]), (90,120,50))
+    # for x in range(pt[0], pt[0]+boxLength):
+    #     for y in range(pt[1], pt[1]+boxLength):
+    #         colours.append(im.getpixel((x,y)))
+    
+
+# for rgb in colours:
+#     ave_r += rgb[0]
+#     ave_g += rgb[1]
+#     ave_b += rgb[2]
+
+# avgRGB = int(ave_r/boxSize),int(ave_g/boxSize),int(ave_b/boxSize)
+# # print(avgRGB)
+# for x in range(pt[0], pt[0]+boxLength):
+#     for y in range(pt[1], pt[1]+boxLength):
+#         im.putpixel((x,y), avgRGB)
 
    
 # new.show()
-im.show()
+new.show()
 # new.save('output/1.png')
 #         new.putpixel((x, y), (255, 255, 255))
         # r,g,b = im.getpixel((x,y))
